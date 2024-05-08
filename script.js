0b864170dffaedd20858d426e6666b997040c7cc
Fiona Peacock <108232027+peacockcounselling@users.noreply.github.com>
2024-05-08 16:40:42 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle sidebar menu
+function toggleMenu() {
+  var sidebar = document.getElementById("mySidebar");
+  if (sidebar.style.display === "block") {
+    sidebar.style.display = "none";
+  } else {
+    sidebar.style.display = "block";
+  }
+}
