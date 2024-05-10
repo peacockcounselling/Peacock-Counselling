c58fa47df8a959b0b2b7a07dc9b6f9003ef89339
Fiona Peacock <108232027+peacockcounselling@users.noreply.github.com>
2024-05-10 16:36:47 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Toggle sidebar menu
-function toggleMenu() {
-  var sidebar = document.getElementById("mySidebar");
-  if (sidebar.style.display === "block") {
-    sidebar.style.display = "none";
-  } else {
-    sidebar.style.display = "block";
-  }
-}
